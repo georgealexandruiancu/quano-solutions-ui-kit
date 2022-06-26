0a0cddba751ba31789f1c85eeb22da0e1cd2e5f4
georgealexandruiancu <iancugeorgealexandru@gmail.com>
2022-06-26 19:40:53 +0300

    playing around with icons and added some generator for them

---
+++ fonts/icons/icons.ts
@@ -0,0 +1,209 @@
+export type IconsId =
+  | "arrow-down"
+  | "arrow-left"
+  | "arrow-right"
+  | "arrow-up"
+  | "backspace"
+  | "bell"
+  | "book"
+  | "bookmark"
+  | "calculator"
+  | "calendar-alt"
+  | "calendar"
+  | "camera-alt"
+  | "camera"
+  | "card"
+  | "case"
+  | "check"
+  | "close"
+  | "copy"
+  | "database"
+  | "decrease"
+  | "denied"
+  | "desktop"
+  | "document-add"
+  | "document-chart"
+  | "document-copy"
+  | "document-download"
+  | "document-remove"
+  | "document-text"
+  | "document"
+  | "dots-alt"
+  | "dots"
+  | "download"
+  | "enlarge"
+  | "external"
+  | "filter"
+  | "finger-print"
+  | "menu"
+  | "mobile"
+  | "money"
+  | "pause"
+  | "pipe"
+  | "play"
+  | "search"
+  | "stop"
+  | "tablet"
+  | "target"
+  | "video"
+  | "view-alt"
+  | "view"
+  | "warning";
+
+export type IconsKey =
+  | "ArrowDown"
+  | "ArrowLeft"
+  | "ArrowRight"
+  | "ArrowUp"
+  | "Backspace"
+  | "Bell"
+  | "Book"
+  | "Bookmark"
+  | "Calculator"
+  | "CalendarAlt"
+  | "Calendar"
+  | "CameraAlt"
+  | "Camera"
+  | "Card"
+  | "Case"
+  | "Check"
+  | "Close"
+  | "Copy"
+  | "Database"
+  | "Decrease"
+  | "Denied"
+  | "Desktop"
+  | "DocumentAdd"
+  | "DocumentChart"
+  | "DocumentCopy"
+  | "DocumentDownload"
+  | "DocumentRemove"
+  | "DocumentText"
+  | "Document"
+  | "DotsAlt"
+  | "Dots"
+  | "Download"
+  | "Enlarge"
+  | "External"
+  | "Filter"
+  | "FingerPrint"
+  | "Menu"
+  | "Mobile"
+  | "Money"
+  | "Pause"
+  | "Pipe"
+  | "Play"
+  | "Search"
+  | "Stop"
+  | "Tablet"
+  | "Target"
+  | "Video"
+  | "ViewAlt"
+  | "View"
+  | "Warning";
+
+export enum Icons {
+  ArrowDown = "arrow-down",
+  ArrowLeft = "arrow-left",
+  ArrowRight = "arrow-right",
+  ArrowUp = "arrow-up",
+  Backspace = "backspace",
+  Bell = "bell",
+  Book = "book",
+  Bookmark = "bookmark",
+  Calculator = "calculator",
+  CalendarAlt = "calendar-alt",
+  Calendar = "calendar",
+  CameraAlt = "camera-alt",
+  Camera = "camera",
+  Card = "card",
+  Case = "case",
+  Check = "check",
+  Close = "close",
+  Copy = "copy",
+  Database = "database",
+  Decrease = "decrease",
+  Denied = "denied",
+  Desktop = "desktop",
+  DocumentAdd = "document-add",
+  DocumentChart = "document-chart",
+  DocumentCopy = "document-copy",
+  DocumentDownload = "document-download",
+  DocumentRemove = "document-remove",
+  DocumentText = "document-text",
+  Document = "document",
+  DotsAlt = "dots-alt",
+  Dots = "dots",
+  Download = "download",
+  Enlarge = "enlarge",
+  External = "external",
+  Filter = "filter",
+  FingerPrint = "finger-print",
+  Menu = "menu",
+  Mobile = "mobile",
+  Money = "money",
+  Pause = "pause",
+  Pipe = "pipe",
+  Play = "play",
+  Search = "search",
+  Stop = "stop",
+  Tablet = "tablet",
+  Target = "target",
+  Video = "video",
+  ViewAlt = "view-alt",
+  View = "view",
+  Warning = "warning",
+}
+
+export const ICONS_CODEPOINTS: { [key in Icons]: string } = {
+  [Icons.ArrowDown]: "61697",
+  [Icons.ArrowLeft]: "61698",
+  [Icons.ArrowRight]: "61699",
+  [Icons.ArrowUp]: "61700",
+  [Icons.Backspace]: "61701",
+  [Icons.Bell]: "61702",
+  [Icons.Book]: "61703",
+  [Icons.Bookmark]: "61704",
+  [Icons.Calculator]: "61705",
+  [Icons.CalendarAlt]: "61706",
+  [Icons.Calendar]: "61707",
+  [Icons.CameraAlt]: "61708",
+  [Icons.Camera]: "61709",
+  [Icons.Card]: "61710",
+  [Icons.Case]: "61711",
+  [Icons.Check]: "61712",
+  [Icons.Close]: "61713",
+  [Icons.Copy]: "61714",
+  [Icons.Database]: "61715",
+  [Icons.Decrease]: "61716",
+  [Icons.Denied]: "61717",
+  [Icons.Desktop]: "61718",
+  [Icons.DocumentAdd]: "61719",
+  [Icons.DocumentChart]: "61720",
+  [Icons.DocumentCopy]: "61721",
+  [Icons.DocumentDownload]: "61722",
+  [Icons.DocumentRemove]: "61723",
+  [Icons.DocumentText]: "61724",
+  [Icons.Document]: "61725",
+  [Icons.DotsAlt]: "61726",
+  [Icons.Dots]: "61727",
+  [Icons.Download]: "61728",
+  [Icons.Enlarge]: "61729",
+  [Icons.External]: "61730",
+  [Icons.Filter]: "61731",
+  [Icons.FingerPrint]: "61732",
+  [Icons.Menu]: "61733",
+  [Icons.Mobile]: "61734",
+  [Icons.Money]: "61735",
+  [Icons.Pause]: "61736",
+  [Icons.Pipe]: "61737",
+  [Icons.Play]: "61738",
+  [Icons.Search]: "61739",
+  [Icons.Stop]: "61740",
+  [Icons.Tablet]: "61741",
+  [Icons.Target]: "61742",
+  [Icons.Video]: "61743",
+  [Icons.ViewAlt]: "61744",
+  [Icons.View]: "61745",
+  [Icons.Warning]: "61746",
+};
